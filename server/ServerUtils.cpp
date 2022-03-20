e5607ca4dd72f4b564afb5b8befcd26d3205579d
priyanshu1998 <priyanshutirkey1998@gmail.com>
2022-03-20 10:12:54 +0530

    commits for event queue

--- server/ServerUtils.cpp
+++ server/ServerUtils.cpp
@@ -7,15 +7,62 @@
 bool ServerUtils::readIP(int fd, char *IP) {
     char c = 0;
     int idx = 0;
-    while (c != '\n') {
+    for(;;){
         if (read(fd, &c, 1) == -1) {
             int errsv = errno;
             fprintf(stderr, "[E| unable to read IP] %s\n", strerror(errsv));
             return false;
         }
-        if(c=='\n'){continue;}
+        if(c=='\n'){ // '\n' is delimiter
+            IP[idx] = 0;
+            break;
+        }
         IP[idx] = c;
         idx++;
     }
     return true;
+}
+
+bool ServerUtils::insertAcceptSock(std::map<std::string, Conn> &peerEndpoints, const char *host, int sockfd){
+    auto it = peerEndpoints.find(host);
+    if(it != peerEndpoints.end()){
+        it->second.acceptSock = sockfd;
+
+        // the fact that it was found implies .connectSock exist
+        // therefore both the listening sockets have established connection.
+
+        return true;
+    }else{
+        peerEndpoints[host] = ServerUtils::Conn{};
+        peerEndpoints[host].acceptSock = sockfd;
+        return false;
+    }
+}
+
+bool ServerUtils::insertConnectSock(std::map<std::string, Conn> &peerEndpoints, const char *host, int sockfd) {
+    auto it = peerEndpoints.find(host);
+    if(it != peerEndpoints.end()){
+        it->second.connectSock = sockfd;
+
+        // the fact that it was found implies .acceptSock exist
+        // therefore both the listening sockets have established connection.
+        return true;
+    }else{
+        peerEndpoints[host] = ServerUtils::Conn{};
+        peerEndpoints[host].connectSock = sockfd;
+        return false;
+    }
+}
+
+int ServerUtils::nextEventDescriptor(std::deque<int> &eventQueue) {
+    int i = eventQueue.front();
+    eventQueue.pop_front();
+    eventQueue.push_back(i);
+
+    return i;
+}
+
+int ServerUtils::addEventDescriptor(std::deque<int> &eventQueue, int fd, fd_set &readfds) {
+    eventQueue.push_back(fd);
+    FD_SET(fd, &readfds);
 }
--- server/ServerUtils.h
+++ server/ServerUtils.h
@@ -9,10 +9,26 @@
 #include <cerrno>
 #include <cstdio>
 #include <cstring>
+#include <map>
+#include <string>
+#include <deque>
 
 namespace ServerUtils{
+    constexpr int TOT_SERVERS = 3;
+
     bool readIP(int fd ,char *IP);
 
+    struct Conn{
+        int acceptSock{-1};
+        int connectSock{-1};
+    };
 
+    bool insertConnectSock(std::map<std::string,Conn> &peerEndpoints, const char *host, int sockfd);
+
+    bool insertAcceptSock(std::map<std::string,Conn> &peerEndpoints, const char *host, int sockfd);
+
+    int nextEventDescriptor(std::deque<int>&eventQueue);
+
+    int addEventDescriptor(std::deque<int>&eventQueue, int fd, fd_set &readfds);
 }
 #endif //RAFT_CPP_SERVERUTILS_H
